8a33223c5dc1d3721b3cdef0df2d42c9da9f80e8
Alok <alok.sharma61630@gmail.com>
2020-07-01 12:34:58 +0530

    Bugs Fixed setup done

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const greet = "Hello Everyone";
+const name = "How are you";
+import { age } from "./example";
+import "../sass/main.scss";
+
+console.log(`${greet}, ${name}, I am ${age} years old`);
